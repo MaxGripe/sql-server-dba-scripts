42b16846e3e476d4d9f46c0575d9e867d5176dfb
Max Gripe 1982 <108034506+MaxGripe@users.noreply.github.com>
2024-10-17 09:24:51 +0200

    Update backups.sql

--- backups.sql
+++ backups.sql
@@ -14,3 +14,24 @@ INNER JOIN msdb.dbo.backupset bs ON rs.backup_set_id = bs.backup_set_id
 INNER JOIN msdb.dbo.backupmediafamily bmf ON bs.media_set_id = bmf.media_set_id
 WHERE rs.destination_database_name = 'my_database_name'
 ORDER BY rs.restore_date DESC;
+
+/*
+	History of backups from the specified database.
+	If @DatabaseName is an empty string, it returns backup history for all databases.
+*/
+DECLARE @DatabaseName NVARCHAR(255);
+SET @DatabaseName = '';
+
+SELECT 
+    bs.media_set_id,
+    bs.backup_finish_date,
+    bs.type,
+    bs.backup_size,
+    bs.compressed_backup_size,
+    mf.physical_device_name
+FROM msdb.dbo.backupset AS bs
+INNER JOIN msdb.dbo.backupmediafamily AS mf
+    ON bs.media_set_id = mf.media_set_id
+WHERE (@DatabaseName = '' OR bs.database_name = @DatabaseName)
+ORDER BY bs.backup_finish_date DESC;
+GO
