26438166250899ab1f4887e08543ff75d980ab6f
Max Gripe 1982 <108034506+MaxGripe@users.noreply.github.com>
2024-11-27 13:40:17 +0100

    Get ETA for ongoing RESTORE

--- backups.sql
+++ backups.sql
@@ -55,3 +55,67 @@ INNER JOIN msdb.dbo.backupmediafamily AS mf
 WHERE (@DatabaseName = '' OR bs.database_name = @DatabaseName)
 ORDER BY bs.backup_finish_date DESC;
 GO
+
+
+
+/*
+	Get ETA for ongoing RESTORE
+*/
+DECLARE @ProgressTable TABLE (
+    DatabaseName NVARCHAR(128),
+    InitialProgress FLOAT,
+    SecondProgress FLOAT,
+    ProgressDifference FLOAT,
+    EstimatedMinutes FLOAT
+);
+INSERT INTO @ProgressTable (DatabaseName, InitialProgress)
+SELECT 
+    d.name AS DatabaseName,
+    r.percent_complete AS InitialProgress
+FROM 
+    sys.dm_exec_requests r
+CROSS APPLY 
+    sys.dm_exec_sql_text(r.sql_handle) AS t
+JOIN 
+    sys.databases d ON t.text LIKE '%' + d.name + '%'
+WHERE 
+    r.command = 'RESTORE DATABASE'
+    AND d.state_desc = 'RESTORING';
+-- Wait 10 sec
+WAITFOR DELAY '00:00:10';
+UPDATE @ProgressTable
+SET 
+    SecondProgress = t.SecondProgress
+FROM 
+    @ProgressTable p
+INNER JOIN (
+    SELECT 
+        d.name AS DatabaseName,
+        r.percent_complete AS SecondProgress
+    FROM 
+        sys.dm_exec_requests r
+    CROSS APPLY 
+        sys.dm_exec_sql_text(r.sql_handle) AS t
+    JOIN 
+        sys.databases d ON t.text LIKE '%' + d.name + '%'
+    WHERE 
+        r.command = 'RESTORE DATABASE'
+        AND d.state_desc = 'RESTORING'
+) t ON p.DatabaseName = t.DatabaseName;
+UPDATE @ProgressTable
+SET 
+    ProgressDifference = SecondProgress - InitialProgress,
+    EstimatedMinutes = CASE 
+        WHEN (SecondProgress - InitialProgress) > 0 THEN 
+            ((100 - SecondProgress) / (SecondProgress - InitialProgress)) * (10.0 / 60.0) -- 10 sec in mins
+        ELSE 
+            NULL
+    END;
+SELECT 
+    DatabaseName,
+    InitialProgress,
+    SecondProgress,
+    ProgressDifference,
+    EstimatedMinutes AS EstimatedTimeRemainingInMinutes
+FROM 
+    @ProgressTable;
